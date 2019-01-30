8d376c3f2a85a27abada334cea8c4c03b51deb31
Xiaochen Lian <lianxiaochen@gmail.com>
2019-01-29 18:09:22 -0800

    backup before copy

--- config.sh
+++ config.sh
@@ -1,3 +1,26 @@
+# Define a timestamp function                                                    
+timestamp() {                                                                    
+  date +"%Y-%m-%d_%H-%M-%S"                                                      
+}                                                                                
+
+TIMESTAMP=$(timestamp)
+                                                                                 
+WD=${1:$HOME/work}
+VENV=$WD/py_cvlab
+
+# vim
+echo "setting up vim"
+cp ~/.vimrc ~/.vimrc.${TIMESTAMP} 
+cp vimrc.sample ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+# bash
+cp ~/.bash_profile ~/.bash_profile.${TIMESTAMP} 
+cat bash_profile.sample > ~/.bash_profile
+# git
+cp ~/.gitconfig ~/.gitconfig.${TIMESTAMP} 
+cat gitconfig.sample > ~/.gitconfig
+
 unameOut="$(uname -s)"
 case "${unameOut}" in
     Linux*)     machine=linux;;
@@ -16,26 +39,21 @@ if [ "${machine}" == "darwin" ]; then
     brew install tmux
     echo "xcode command line tools"
     xcode-select --install
-    # pip
     echo "pip"
     sudo easy_install pip
     # clang-format
     echo "clang-format"
     brew install clang-format
+    # install system-wide python libraries
+    echo "python futures"
+    sudo pip install futures
+    echo "python yapf"
+    sudo pip install yapf
+    # pre-commit
+    echo "pre-commit"
+    sudo pip install pre-commit
 elif [ "${machine}" == "linux" ]; then
-    # tmux
-    echo "tmux"
-    sudo apt-get install tmux    
+    mkdir -p $VENV
+    virtualenv $VENV
+    cd $WD
 fi
-
-# install system-wide python libraries
-echo "python futures"
-sudo pip install futures
-echo "python yapf"
-sudo pip install yapf
-# pre-commit
-echo "pre-commit"
-sudo pip install pre-commit
-
-cat bash_profile.sample > ~/.bash_profile
-cat gitconfig.sample > ~/.gitconfig
